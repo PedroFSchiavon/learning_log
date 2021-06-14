9c39e11b5e39d13b1beb2345900607c08217678a
PedroFSchiavon <pedrofschiavon@gmail.com>
2021-06-14 17:49:45 -0300

    Criando o Model Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    '''Local onde será inserido oq o usuário esta aprendendo'''
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        '''Retorna uma representação em string do modelo.'''
+        return self.text
